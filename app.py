359fff7ae2873c47537d66383b02c0128b155255
syahruaru <57306432+syahruaru@users.noreply.github.com>
2023-05-25 15:40:05 +0700

    app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ st.set_page_config(
     layout = "wide"
 )
 
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQMqC_6fkaH6oZweJDIIYFDdE9o3P3G1hB0OKLzkGGf0pB-FjWJoAMoYca2iXV2ID5dE7hoklCSx6hE/pub?gid=0&single=true&output=csv
-')
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQMqC_6fkaH6oZweJDIIYFDdE9o3P3G1hB0OKLzkGGf0pB-FjWJoAMoYca2iXV2ID5dE7hoklCSx6hE/pub?gid=0&single=true&output=csv')
 df['order_date'] = pd.to_datetime(df['order_date'])
 df['ship_date'] = pd.to_datetime(df['ship_date'])
 
